4f867d57e33d701656ac53e1d2541dec0a2e3b24
softgraph <rr@sgraph.com>
2019-12-12 23:08:19 +0900

    Added `gesture::microBitGestureEventTilt()` and updated functions in namespace `gesture`

--- inc/ExtKitGesture.h
+++ inc/ExtKitGesture.h
@@ -13,22 +13,53 @@
 #define EXT_KIT_GESTURE_H
 
 #include "ExtKit_Common.h"
+#include "ExtKitDirection.h"
 
 namespace microbit_dal_ext_kit {
 
 /// Gesture utility
 namespace gesture {
 
-/// Event value for MICROBIT_ID_GESTURE. Returns MICROBIT_ACCELEROMETER_EVT_TILT_LEFT (tilt to the left) if display::displayRotation() is MICROBIT_DISPLAY_ROTATION_0.
+/// MICROBIT_ID_GESTURE event for tilt to a directiction
+/**
+ * @param tiltDdirection one of the following directions
+ * - direction::kN
+ * - direction::kE
+ * - direction::kW
+ * - direction::kS
+ * .
+ * @returns one of the following events depending on the configuration set by display::setDisplayRotation()
+ * - MICROBIT_ACCELEROMETER_EVT_TILT_LEFT ... tilt to the left (A buton side of the micro:bit)
+ * - MICROBIT_ACCELEROMETER_EVT_TILT_DOWN ... tilt to the top (USB connector side of the micro:bit)
+ * - MICROBIT_ACCELEROMETER_EVT_TILT_RIGHT ... tilt to the right (B button side of the micro:bit)
+ * - MICROBIT_ACCELEROMETER_EVT_TILT_UP ... tilt to the bottom (edge connector side of the micro:bit)
+ * - MICROBIT_ACCELEROMETER_EVT_NONE
+ * .
+ */
+int microBitGestureEventTilt(Direction tiltDdirection);
+
+/// MICROBIT_ID_GESTURE event for tilt to the logical left (west)
+/**
+ * @deprecated This method is obsolete and will be removed in a future release. Please use `microBitGestureEventTilt()` with `direction::kW` instead.
+ */
 int microBitGestureEventTiltLeft();
 
-/// Event value for MICROBIT_ID_GESTURE. Returns MICROBIT_ACCELEROMETER_EVT_TILT_DOWN (tilt to the top) if display::displayRotation() is MICROBIT_DISPLAY_ROTATION_0.
+/// MICROBIT_ID_GESTURE event for tilt to the logical top (north)
+/**
+ * @deprecated This method is obsolete and will be removed in a future release. Please use `microBitGestureEventTilt()` with `direction::kN` instead.
+ */
 int microBitGestureEventTiltTop();
 
-/// Event value for MICROBIT_ID_GESTURE. Returns MICROBIT_ACCELEROMETER_EVT_TILT_RIGHT (tilt to the right) if display::displayRotation() is MICROBIT_DISPLAY_ROTATION_0.
+/// MICROBIT_ID_GESTURE event for tilt to the logical right (east)
+/**
+ * @deprecated This method is obsolete and will be removed in a future release. Please use `microBitGestureEventTilt()` with `direction::kE` instead.
+ */
 int microBitGestureEventTiltRight();
 
-/// Event value for MICROBIT_ID_GESTURE. Returns MICROBIT_ACCELEROMETER_EVT_TILT_UP (tilt to the bottom) if display::displayRotation() is MICROBIT_DISPLAY_ROTATION_0.
+/// MICROBIT_ID_GESTURE event for tilt to the logical bottom (south)
+/**
+ * @deprecated This method is obsolete and will be removed in a future release. Please use `microBitGestureEventTilt()` with `direction::kS` instead.
+ */
 int microBitGestureEventTiltBottom();
 
 }	// gesture
--- source/ExtKitGesture.cpp
+++ source/ExtKitGesture.cpp
@@ -15,47 +15,76 @@
 namespace microbit_dal_ext_kit {
 namespace gesture {
 
+/// @cond static
+
+/// Indices and count for events
+enum EventIndex
+{
+	kEventN,	///< N for MICROBIT_DISPLAY_ROTATION_0
+	kEventW,	///< W for MICROBIT_DISPLAY_ROTATION_0 (or N for MICROBIT_DISPLAY_ROTATION_90)
+	kEventS,	///< S for MICROBIT_DISPLAY_ROTATION_0 (or N for MICROBIT_DISPLAY_ROTATION_180)
+	kEventE,	///< E for MICROBIT_DISPLAY_ROTATION_0 (or N for MICROBIT_DISPLAY_ROTATION_270)
+	kEventCount	///< Count
+
+};	// EventIndex
+
+/// @endcond static
+
+static int sMicroBitGestureEvents[kEventCount] = {
+	MICROBIT_ACCELEROMETER_EVT_TILT_DOWN,	// [kEventN]
+	MICROBIT_ACCELEROMETER_EVT_TILT_LEFT, 	// [kEventW]
+	MICROBIT_ACCELEROMETER_EVT_TILT_UP,		// [kEventS]
+	MICROBIT_ACCELEROMETER_EVT_TILT_RIGHT	// [kEventE]
+};
+
+int microBitGestureEventTilt(Direction tiltDdirection)
+{
+	if(display::isBackToFront()) {
+		switch(tiltDdirection) {
+			case direction::kN: { tiltDdirection = direction::kS; break; }
+			case direction::kE: { tiltDdirection = direction::kW; break; }
+			case direction::kW: { tiltDdirection = direction::kE; break; }
+			case direction::kS: { tiltDdirection = direction::kN; break; }
+			default: { break; }
+		}
+	}
+
+	int /* EventIndex */ eventIndex = -1;
+	switch(tiltDdirection) {
+		case direction::kN: { eventIndex = kEventN; break; }
+		case direction::kE: { eventIndex = kEventE; break; }
+		case direction::kW: { eventIndex = kEventW; break; }
+		case direction::kS: { eventIndex = kEventS; break; }
+		default: { break; }
+	}
+
+	if(0 <= eventIndex) {
+		eventIndex += display::displayRotation();
+		eventIndex %= kEventCount;
+		return sMicroBitGestureEvents[eventIndex];
+	}
+	else {
+		return MICROBIT_ACCELEROMETER_EVT_NONE;
+	}
+}
+
 int microBitGestureEventTiltLeft()
 {
-	switch(display::displayRotation()) {
-		default:
-		case MICROBIT_DISPLAY_ROTATION_0:	return MICROBIT_ACCELEROMETER_EVT_TILT_LEFT;
-		case MICROBIT_DISPLAY_ROTATION_90:	return MICROBIT_ACCELEROMETER_EVT_TILT_DOWN;	// = TOP
-		case MICROBIT_DISPLAY_ROTATION_180:	return MICROBIT_ACCELEROMETER_EVT_TILT_RIGHT;
-		case MICROBIT_DISPLAY_ROTATION_270:	return MICROBIT_ACCELEROMETER_EVT_TILT_UP;		// = BOTTOM
-	}
+	return microBitGestureEventTilt(direction::kW);
 }
 
 int microBitGestureEventTiltTop()
 {
-	switch(display::displayRotation()) {
-		case MICROBIT_DISPLAY_ROTATION_270:	return MICROBIT_ACCELEROMETER_EVT_TILT_LEFT;
-		default:
-		case MICROBIT_DISPLAY_ROTATION_0:	return MICROBIT_ACCELEROMETER_EVT_TILT_DOWN;	// = TOP
-		case MICROBIT_DISPLAY_ROTATION_90:	return MICROBIT_ACCELEROMETER_EVT_TILT_RIGHT;
-		case MICROBIT_DISPLAY_ROTATION_180:	return MICROBIT_ACCELEROMETER_EVT_TILT_UP;		// = BOTTOM
-	}
+	return microBitGestureEventTilt(direction::kN);
 }
 
 int microBitGestureEventTiltRight()
 {
-	switch(display::displayRotation()) {
-		case MICROBIT_DISPLAY_ROTATION_180:	return MICROBIT_ACCELEROMETER_EVT_TILT_LEFT;
-		case MICROBIT_DISPLAY_ROTATION_270:	return MICROBIT_ACCELEROMETER_EVT_TILT_DOWN;	// = TOP
-		default:
-		case MICROBIT_DISPLAY_ROTATION_0:	return MICROBIT_ACCELEROMETER_EVT_TILT_RIGHT;
-		case MICROBIT_DISPLAY_ROTATION_90:	return MICROBIT_ACCELEROMETER_EVT_TILT_UP;		// = BOTTOM
-	}
+	return microBitGestureEventTilt(direction::kE);
 }
 int microBitGestureEventTiltBottom()
 {
-	switch(display::displayRotation()) {
-		case MICROBIT_DISPLAY_ROTATION_90:	return MICROBIT_ACCELEROMETER_EVT_TILT_LEFT;
-		case MICROBIT_DISPLAY_ROTATION_180:	return MICROBIT_ACCELEROMETER_EVT_TILT_DOWN;	// = TOP
-		case MICROBIT_DISPLAY_ROTATION_270:	return MICROBIT_ACCELEROMETER_EVT_TILT_RIGHT;
-		default:
-		case MICROBIT_DISPLAY_ROTATION_0:	return MICROBIT_ACCELEROMETER_EVT_TILT_UP;		// = BOTTOM
-	}
+	return microBitGestureEventTilt(direction::kS);
 }
 
 }	// gesture
